08f55429594a6995f3c9d4aa36f52369862b25e6
HJulie11 <Hseojin0711@gmail.com>
2023-08-02 01:07:04 +0900

    html, css 파일 보여주기 진행중 - module not found error

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const helmet = require('helmet');
 const app = express();
+const ejs = require("ejs");
+
+//HTML,CSS 파일 보여주기
+app.set('view engine', 'ejs');
+app.set('view', './views');
+app.use('/public', express.static(__dirname + '/public'));
+
 app.use(helmet());
+//post api 실행을 위한 lines
+app.use(express.json());
+app.use(express.urlencoded());
 
 const mainRouter = require('./router/mainRouter');
 app.use('/', mainRouter);
--- router/mainRouter.js
+++ router/mainRouter.js
@@ -3,11 +3,18 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/", function(req,res){
-  res.send('Hello World');
+  //그림파일 전달
+  res.render('indextest')
 })
 
 router.get("/about", function(req,res){
   res.send('About Page');
 }) 
+
+router.post("/postapi", function(req,res){
+  let body = req.body;
+  console.log(body);
+  res.send('POST API')
+})
 
 module.exports = router;
